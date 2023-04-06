dd03ef289822957ffde21618e049264a8005ac7c
alper bobaj <alperbobaj@alpers-mbp-2.myfiosgateway.com>
2023-04-05 21:23:16 -0400

    Dana has been added

---
+++ src/test/java/Dana/OpenBrowser.java
@@ -0,0 +1,17 @@
+package Dana;
+
+import io.cucumber.java.en.Given;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenBrowser {
+    WebDriver driver;
+    @Given("open the browser and launch HRMS application")
+    public void open_the_browser_and_launch_hrms_application() {
+        driver=new ChromeDriver();
+        driver.get("http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login");
+
+    }
+
+    }
+
