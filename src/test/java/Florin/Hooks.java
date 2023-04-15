d393405241ff1eadc759a6389d6d53757860dd04
florinvardari <florin.vardari@gmail.com>
2023-04-14 20:57:37 -0400

    added the hooks

---
+++ src/test/java/Florin/Hooks.java
@@ -0,0 +1,16 @@
+package Florin;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks {
+    @Before // make sure its from  io.cucumber.java.Before;
+    public void preCondition(){
+
+    }
+
+    @After  // make sure its from  io.cucumber.java.After;
+    public void postCondition(){
+
+    }
+}
